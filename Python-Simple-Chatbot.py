abaca8bb27235b96926f90091f9bcf672bcf43c7
Keenan Kelly <keenank22@yahoo.com>
2025-07-04 14:12:11 -0500

    Initial commit

---
+++ Python-Simple-Chatbot.py
@@ -0,0 +1,66 @@
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
+
+
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+
+    question = "Why do we use methods in programming?\n" \
+               "1. To repeat a statement multiple times.\n" \
+               "2. To decompose a program into several small subroutines.\n" \
+               "3. To determine the execution time of a program.\n" \
+               "4. To interrupt the execution of a program?"
+
+    correct_answer = 2  # The correct option is 2
+
+    while True:
+        print(question)
+        user_answer = input("> ").strip()
+
+        if user_answer.isdigit() and int(user_answer) == correct_answer:
+            print("Congratulations, have a nice day!")
+            break
+        else:
+            print("Please, try again.")  # write your code here
+    print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Enzi', '2024')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()  # ...
+end()
